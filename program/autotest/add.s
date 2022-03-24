bf87e8d0895674a7fa1d22c3877fbf5c96a57103
verninp <paul.vernin@grenoble-inp.org>
2022-03-24 10:38:11 +0100

    add

---
+++ program/autotest/add.s
@@ -0,0 +1,10 @@
+# TAG = add
+	.text
+	add x31, x0, x0
+	addi x1, x0, 52
+	add x31, x31, x1
+	# max_cycle 50
+	# pout_start
+	# 00000000
+	# 00000034
+	# pout_end
--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -2,8 +2,10 @@
 	.text
 	addi x31, x0, 1 
 	addi x31, x31, 0xfffffffe
+	addi x31, x31, 1
 	# max_cycle 50
 	# pout_start
 	# 00000001
 	# ffffffff
+	# 00000000
 	# pout_end
