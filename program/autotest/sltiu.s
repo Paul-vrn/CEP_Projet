ee1447f84e9448fbfebe2e28390e828233b2b28a
verninp <paul.vernin@grenoble-inp.org>
2022-04-05 17:45:53 +0200

    modif des tests

--- program/autotest/sltiu.s
+++ program/autotest/sltiu.s
@@ -2,12 +2,15 @@
 	.text
 	addi x28, x0, 5
 	addi x29, x0, 6
-	sltiu x31, x28, 6
+	addi x27, x0, -6
+	sltiu x31, x28, -6
 	sltiu x31, x29, 5
 	sltiu x31, x29, 6
+	sltiu x31, x27, 6
 	# max_cycle 50
 	# pout_start
 	# 00000001
+	# 00000000
 	# 00000000
 	# 00000000
 	# pout_end
--- program/autotest/sra.s
+++ program/autotest/sra.s
@@ -2,15 +2,15 @@
 	.text
 	addi x30, x0, 4
 	addi x31, x0, 0xffffffff
-	srl x31, x31, x30
-	srl x31, x31, x30
-	srl x31, x31, x30
-	srl x31, x31, x30
+	sra x31, x31, x30
+	sra x31, x31, x30
+	sra x31, x31, x30
+	sra x31, x31, x30
 	# max_cycle 50
 	# pout_start
 	# ffffffff
-	# 0fffffff
-	# 00ffffff
-	# 000fffff
-	# 0000ffff
+	# ffffffff
+	# ffffffff
+	# ffffffff
+	# ffffffff
 	# pout_end
