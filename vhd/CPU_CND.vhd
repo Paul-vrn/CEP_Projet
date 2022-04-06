acdf80a533b7ff11daa799d34e5fa622a81013c7
verninp <paul.vernin@grenoble-inp.org>
2022-04-06 10:46:02 +0200

    CPU_CND fixed

--- vhd/CPU_CND.vhd
+++ vhd/CPU_CND.vhd
@@ -17,14 +17,17 @@ entity CPU_CND is
 end entity;
 
 architecture RTL of CPU_CND is
-    signal res : unsigned(32 downto 0);
+    signal res, rs1_temp, ALU_Y_temp : signed(32 downto 0);
     signal ext : std_logic;
     signal z,s : std_logic;
 begin
 
-ext <= (IR(12) nor IR(6)) or (IR(6) and IR(13));
-res <= ((ext and rs1(31)) & rs1) - ((ext and alu_y(31)) & alu_y);
+ext <= (IR(12) nor IR(6)) or (IR(6) and not IR(13));
 
+rs1_temp <= signed(rs1(31)&rs1) when ext='1' else signed('0' &rs1);
+ALU_Y_temp <= signed(ALU_Y(31)&ALU_Y) when ext='1' else signed('0'&ALU_Y);
+
+res <= signed(rs1_temp) - signed(ALU_Y_temp);
 
 z <= '1' when (res="0") else '0';
 jcond <= ((z xor IR(12)) and (not IR(14))) or ( (res(32) xor IR(12)) and IR(14));
