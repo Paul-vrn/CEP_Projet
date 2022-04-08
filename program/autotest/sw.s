7b9d45f02603ae5d8bb7e314033fce97eb1b81de
Paul Vernin <paul.vernin@grenoble-inp.org>
2022-04-09 00:10:27 +0200

    test STORE fix

--- program/autotest/sw.s
+++ program/autotest/sw.s
@@ -1,14 +1,14 @@
 # TAG = sw
 	.text
 	la x1, test
-	addi x31, x0, 0xcafecafe
+	addi x31, x0, 0xcafe
 	sw x31, 0(x1)
 	addi x31, x31, 1
 	lw x31, 0(x1)
 	# max_cycle 50
-	# cafecafe
-	# cafecaff
-	# cafecafe
+	# 0000cafe
+	# 0000caff
+	# 0000cafe
 	# pout_end
 
 	.data
