8ac3a2a2b1c00a3d2f3810cd844f1b7cd747476c
verninp <paul.vernin@grenoble-inp.org>
2022-04-18 13:09:52 +0200

    test

--- vhd/CPU_CSR.vhd
+++ vhd/CPU_CSR.vhd
@@ -60,7 +60,6 @@ architecture RTL of CPU_CSR is
 begin
     to_csr <= rs1 when cmd.TO_CSR_sel = TO_CSR_from_rs1 else imm;  
 
-    it <= irq and mstatus(3);
 
     csr <=  mcause_q when cmd.CSR_sel = CSR_from_mcause else
             mepc when cmd.CSR_sel = CSR_from_mepc else
@@ -69,19 +68,12 @@ begin
             mie when cmd.CSR_sel = CSR_from_mie else
             mip when cmd.CSR_sel = CSR_from_mip;
         
+            it <= irq and mstatus(3);
 
     process (clk)
     begin
         if rising_edge(clk) then
-            if (cmd.CSR_we=CSR_mtvec) then
-                mtvec <= CSR_write(to_csr, mtvec, cmd.CSR_write_mode);
-            elsif (cmd.CSR_we=CSR_mepc) then
-                mepc  <= CSR_write(pc, mepc, cmd.CSR_write_mode);
-            elsif (cmd.CSR_we=CSR_mie) then
-                mie <= CSR_write(to_csr, mie, cmd.CSR_write_mode);
-            elsif (cmd.CSR_we=CSR_mstatus) then
-                mstatus <= CSR_write(to_csr, mstatus, cmd.CSR_write_mode);
-            end if;
+
             if irq = '1' then
                 mcause_q <= mcause;
             end if;
@@ -89,6 +81,31 @@ begin
             mip(7) <= mtip;
             mip(11) <= meip;
         
+            
+--            if (cmd.CSR_we=CSR_mtvec) then
+--                mtvec <= CSR_write(to_csr, mtvec, cmd.CSR_write_mode);
+--            elsif (cmd.CSR_we=CSR_mepc) then
+--                mepc  <= CSR_write(pc, mepc, cmd.CSR_write_mode);
+--            elsif (cmd.CSR_we=CSR_mie) then
+--                mie <= CSR_write(to_csr, mie, cmd.CSR_write_mode);
+--            elsif (cmd.CSR_we=CSR_mstatus) then
+--                mstatus <= CSR_write(to_csr, mstatus, cmd.CSR_write_mode);
+--            end if;
+            case cmd.CSR_we is 
+            when CSR_mepc =>
+                if cmd.MEPC_sel = MEPC_from_csr then
+                    mepc <= CSR_write(TO_CSR, mepc, cmd.CSR_WRITE_mode)(31 downto 2)&"00";
+                elsif cmd.MEPC_sel = MEPC_from_pc then
+                    mepc <= CSR_write(pc, mepc, cmd.CSR_WRITE_mode)(31 downto 2)&"00";
+                end if;
+            when CSR_mtvec =>
+                mtvec <= CSR_write(to_csr, mtvec, cmd.CSR_WRITE_mode)(31 downto 2)&"00"; 
+            when CSR_mie =>
+                mie <= CSR_write(to_csr, mie, cmd.CSR_WRITE_mode);
+            when CSR_mstatus =>
+                mstatus <= CSR_write(to_csr, mstatus, cmd.CSR_WRITE_mode);
+            when others => null;
+            end case;
             if cmd.MSTATUS_mie_set = '1' then
                 mstatus(3) <= '1';
             end if;
